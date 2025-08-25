0e42c1738ab2f9eec9aff1c20cbf2da8a970e5a2
Bruno Timóteo <bruno.timoteo.saber@gmail.com>
2025-08-25 17:24:48 -0300

    Commit inicial 07

---
+++ h0mbre/Exercicio-07.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) 
+{
+    float A, B, C;
+    
+    
+    printf("Insira o valor de A: ");
+    scanf("%f", &A);
+    printf("Insira o valor de B: ");
+    scanf("%f", &B);
+    printf("Insira o valor de C: ");
+    scanf("%f", &C);
+    
+    delta = b*b - 4*a*c
+    
+    x1 = (-b + sqrt(delta))/2a
+    x2 = (-b - sqrt(delta))/2a
+    
+    
+
+    return 0;
+}
