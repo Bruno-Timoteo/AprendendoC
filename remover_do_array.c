e20116789ac17771d6bf2f3985ba15e491accbed
Bruno Timóteo <bruno.timoteo.saber@gmail.com>
2025-09-15 17:14:54 -0300

    V2 com realloc e modularização
    Nesta segunda versão busquei criar mais funções para o código, tirando tudo da main().
    
    Também utilizei realloc como alternativa a criação de um segundo array.

--- remover_do_array.c
+++ remover_do_array.c
@@ -1,49 +1,44 @@
-/* Este algoritmo busca remover um item de um array em C.
+/* Segunda versão do algoritmo:
 
-Ao pesquisar mais sobre o assunto, procurando algo como um equivalente de pop() no Python,
-encontrei que, para remover algum índice de um array, é necessário mover todos os itens
-para uma posição anterior.
+- Modularização em funções
+- Substituir a criação de um segundo array por malloc
+- Loop para repetir várias vezes
 
-Neste código busco gerar a minha implementação deste problema, mas ela provavelmente
-não será boa/eficiente. Após isso, compararei com outros códigos feitos */
+*/
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
-int main()
+void imprimir_array(int arr[], int tamanho)
 {
-    int i, remover, array_usuario[5], len_arr;
+    int i;
+    
+    for (i=0; i<tamanho; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+}
+
+int verificar_input(int tamanho_array)
+{
+    int remover;
     bool invalido;
     
-    for (i=0; i<5; i++)
+    if(tamanho_array == 0)
     {
-        printf("Insira o número que vai na posição %d: ", i+1);
-        scanf("%d", &array_usuario[i]);
-        printf("\n");
+        printf("Não há mais posições a serem removidas");
+        return -1;
     }
     
-    printf("O seu array está como:\n");
-
-    for (i=0; i<5; i++)
-    {
-        printf("%d\n", array_usuario[i]);
-    }
-    
-    len_arr = sizeof(array_usuario)/sizeof(int);
-    
-    if(len_arr == 0)
-    {
-        printf("Não há mais posições para serem removidas");
-        return 0;
-    }
     do
     {
         invalido = false;
-        printf("Insira o índice do número que deseja remover: ");
+        printf("Insira a posição do número que deseja remover: ");
         scanf("%d", &remover);
         printf("\n");
         
-        if(remover >= len_arr || remover < 0)
+        if(remover > tamanho_array || remover < 1)
         {
             printf("Valor inválido! Tente novamente.\n");
             invalido =  true;
@@ -51,28 +46,81 @@ int main()
         
     } while(invalido);
     
-    
-    for (i=remover; i<len_arr-1; i++)
-    {
-        array_usuario[i] = array_usuario[i+1];
-    }
-    
+    return remover-1;
+}
 
-    /* Problema: a última posição não é apagada */
+int main()
+{
+    int i, remover, len_arr=5;
+    char continuar;
     
-    /* Solução 1: criar outro array com uma posição a menos */
+    int* array_usuario = malloc(5 * sizeof(int));
     
-    int array_final[len_arr-1];
-    
-    printf("O seu array final está como:\n");
-    
-    for (i=0; i<len_arr-1; i++)
+    for (i=0; i<5; i++)
     {
-        array_final[i] = array_usuario[i];
-        
-        printf("%d\n", array_final[i]);
+        printf("\nInsira o número que vai na posição %d: ", i+1);
+        scanf("%d", &array_usuario[i]);
+        printf("\n");
     }
     
+    do
+    {
+        
+        printf("O seu array está como:\n");
+    
+        imprimir_array(array_usuario, len_arr);
+        
+        remover = verificar_input(len_arr);
+        
+        if(remover == -1)
+        {
+            break;
+        }
+        
+        len_arr -= 1;
+        
+        /* Lógica para remoção */
+        
+        for (i=remover; i<len_arr; i++)
+        {
+            array_usuario[i] = array_usuario[i+1];
+        }
+        
+        /* Problema: a última posição não é apagada */
+        
+        /* Solução 2: realocar memória (com verificação se diferente de NULL) */
+        
+        int* temp = realloc(array_usuario, sizeof(int) * len_arr);
+        if (temp != NULL) 
+        {
+            array_usuario = temp;
+        }
+        
+        else 
+        {
+            printf("Erro ao realocar memória.\n");
+            free(array_usuario);
+            return 1; // encerra com erro
+        }
+        
+        
+        printf("O seu array após a remoção está como:\n");
+        imprimir_array(array_usuario, len_arr);
+        
+
+        printf("\nVocê deseja remover outra posição (S/N)? ");
+        scanf(" %c", &continuar);
+        printf("\n");
+        
+        /* Limpar o buffer do scanf. Seria interessante colocar outra função que não tem esse problema. */
+
+        int c;
+        while ((c = getchar()) != '\n' && c != EOF);
+        
+    }while(continuar == 'S' || continuar == 's');
+    
+    free(array_usuario);
     
     return 0;
 }
+
