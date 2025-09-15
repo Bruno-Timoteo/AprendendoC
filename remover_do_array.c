4f2e4551857292606c90849b67a66d961d4b151f
Bruno Timóteo <bruno.timoteo.saber@gmail.com>
2025-09-15 16:04:48 -0300

    Versão 1
    Bem rudimentar e não eficiente, mas funciona. Tem verificação de input.

--- remover_do_array.c
+++ remover_do_array.c
@@ -8,11 +8,12 @@ Neste código busco gerar a minha implementação deste problema, mas ela provav
 não será boa/eficiente. Após isso, compararei com outros códigos feitos */
 
 #include <stdio.h>
-
+#include <stdbool.h>
 
 int main()
 {
-    int i, remover, array_usuario[5];
+    int i, remover, array_usuario[5], len_arr;
+    bool invalido;
     
     for (i=0; i<5; i++)
     {
@@ -28,6 +29,50 @@ int main()
         printf("%d\n", array_usuario[i]);
     }
     
+    len_arr = sizeof(array_usuario)/sizeof(int);
+    
+    if(len_arr == 0)
+    {
+        printf("Não há mais posições para serem removidas");
+        return 0;
+    }
+    do
+    {
+        invalido = false;
+        printf("Insira o índice do número que deseja remover: ");
+        scanf("%d", &remover);
+        printf("\n");
+        
+        if(remover >= len_arr || remover < 0)
+        {
+            printf("Valor inválido! Tente novamente.\n");
+            invalido =  true;
+        }
+        
+    } while(invalido);
     
     
+    for (i=remover; i<len_arr-1; i++)
+    {
+        array_usuario[i] = array_usuario[i+1];
+    }
+    
+
+    /* Problema: a última posição não é apagada */
+    
+    /* Solução 1: criar outro array com uma posição a menos */
+    
+    int array_final[len_arr-1];
+    
+    printf("O seu array final está como:\n");
+    
+    for (i=0; i<len_arr-1; i++)
+    {
+        array_final[i] = array_usuario[i];
+        
+        printf("%d\n", array_final[i]);
+    }
+    
+    
+    return 0;
 }
